395fbd0641f24af8a5207185dadef42adc21dead
ProHack <mail@prohack.tech>
2019-08-07 14:02:33 +0800

    Update v1009 final
    + add argument variable to core > config.py
    + remove global config variables from fp.py

---
+++ core/config.py
@@ -0,0 +1,13 @@
+# global variable with default values
+# change if you don't want to pass arguments all the time
+arg_type = '' # proxy type
+arg_anonymity = '' # anonymity type
+arg_country = '' # country code
+arg_filename = '' # custom filename for proxy type
+arg_limit = 500 # limit of proxies to save
+arg_proxyurl = '' # url for proxy browser or for checking proxies against
+arg_pb_timesec = 1000 # timesec to keep browser window alive
+arg_maxbrowsers = 10 # maximum browser to open. Default 10
+arg_youtubevideo = False # play youtube video
+arg_nocheck = False # do not check proxies -> faster
+arg_browsermode = 'normal' # 'normal' or 'headless'
--- fp.py
+++ fp.py
@@ -10,23 +10,10 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from core.colors import c_white, c_green, c_red, c_yellow, c_blue
+from core.config import *
 
 # store proxy information
 proxy_ips, proxy_ports = [], []
-
-# global variable with default values
-# change if you don't want to pass arguments all the time
-arg_type = '' # proxy type
-arg_anonymity = '' # anonymity type
-arg_country = '' # country code
-arg_filename = '' # custom filename for proxy type
-arg_limit = 500 # limit of proxies to save
-arg_proxyurl = '' # url for proxy browser or for checking proxies against
-arg_pb_timesec = 1000 # timesec to keep browser window alive
-arg_maxbrowsers = 10 # maximum browser to open. Default 10
-arg_youtubevideo = False # play youtube video
-arg_nocheck = False # do not check proxies -> faster
-arg_browsermode = 'normal' # 'normal' or 'headless'
 
 # clear screen
 def ClrScrn():
